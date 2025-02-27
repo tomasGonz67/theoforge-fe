0fa725b52d2cbd6e0678ad7d5c90931b8cd252e1
Tomas Gonzalez <tpg3@njit.edu>
2025-02-26 19:52:41 -0500

    setting up dev ops stuff

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,14 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0', // Allow external access
+    port: 8000,      // Ensure it matches Docker's exposed port
+    strictPort: true,
+  },
+  preview: {
+    allowedHosts: ['theoforge.com', 'www.theoforge.com', '0.0.0.0'],
+  },
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
