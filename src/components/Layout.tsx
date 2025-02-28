10f67fefe2c3c313f60c5adaa0273c6edfce5f94
Samuel-okongo <so274@njit.edu>
2025-02-27 20:08:16 -0500

    finished it

---
+++ src/components/Layout.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Sidebar from './Sidebar';
+
+const Layout: React.FC = () => {
+  return (
+    <div className="flex h-screen overflow-hidden">
+      {/* Sidebar */}
+      <Sidebar />
+      
+      {/* Main Content */}
+      <div className="flex-1 overflow-auto">
+        <main className="p-6">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
