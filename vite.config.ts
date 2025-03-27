3eb72e02cc61d3a15c79234b104c61de93e65436
Tomas Gonzalez <tpg3@njit.edu>
2025-03-27 03:00:30 -0400

    Im either crying or sleeping

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import EnvironmentPlugin from 'vite-plugin-environment';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +10,7 @@ export default defineConfig({
   preview: {
     allowedHosts: ['dev.theoforge.com', 'www.dev.theoforge.com', 'qa.theoforge.com', 'www.qa.theoforge.com', 'theoforge.com' ,'www.theoforge.com'],
   },
-  plugins: [react(), EnvironmentPlugin('all')],
+  plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
